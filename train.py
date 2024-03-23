928b501e21d4e3dc99d3ea7aa4449da9c51fcef0
cmsanchi <cm.sanchi@gmail.com>
2024-03-23 18:19:37 +0530

    Introduced error for both actions failures

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import pickle
 import numpy as np
 
 df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease']).to_numpy()
-y = df['Disease'].to_numpy()
+X = df.drop(columns=['Disease_Error_case']).to_numpy()
+y = df['Disease_Error_case'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
